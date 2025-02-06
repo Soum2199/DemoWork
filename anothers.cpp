05610572976ada9f23095696e96ce4adfb1c4df3
Soum2199 <s1646517887@gmail.com>
2025-02-06 13:26:23 +0530

    New C plus file

---
+++ anothers.cpp
@@ -0,0 +1,21 @@
+// Powers of two Coderbyte
+#include <iostream>
+#include <string>
+using namespace std;
+
+int PowersofTwo(int num) {
+  
+  // code goes here  
+  // return num;
+  if(num == 1) return true;
+  if(num<=0 || num%2!=0) return false;
+  return PowersofTwo(num/2);
+}
+
+// keep this function call here
+int main(void) { 
+   
+  cout << PowersofTwo(coderbyteInternalStdinFunction(stdin));
+  return 0;
+    
+}
