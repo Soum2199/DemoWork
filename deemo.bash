68a095e89c5e7b4f601ee778b9e0d7e83500b4a6
Soum2199 <s1646517887@gmail.com>
2025-02-10 16:18:49 +0530

    My bash file got modified

--- deemo.bash
+++ deemo.bash
@@ -2,6 +2,7 @@ Software Engineer has an amazing career path wherein after achieving technical a
 
 For early levels, getting promoted can take anywhere between one to two years as you gain experience. Here's what I did to earn my first promotion and what you should be doing to get promoted, increase your compensation, and open you up to more job opportunities.
 
+Dynamic and motivated individual with a passion for software development and a strong foundation in JavaScript and React.js.
 
 nodemon : File C:\Users\SUBRAT_PANIGRAHI\AppData\Roaming\npm\nodemon.ps1 cannot be loaded because running scripts is 
 disabled on this system. For more information, see about_Execution_Policies at 
