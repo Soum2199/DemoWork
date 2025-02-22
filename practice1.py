b4775d9d92f1d9bab858109e3b593fbc75935904
Soum2199 <s1646517887@gmail.com>
2025-02-22 12:51:35 +0530

    Another files added

---
+++ practice1.py
@@ -0,0 +1,46 @@
+# Some unnamed scenes from python be like:
+
+# print('result: ', 3>2)
+# a = 1976
+# b = "1976"
+# c = 6791
+# d = 1976.0
+# e = True
+# f = 'True'
+# print(a is d)
+# print(type(a), type(b), type(c), type(d), type(e), type(f))
+# print(e is not f)
+
+hey = [1,2,3,4]
+hie = {1,2,3,4}
+print(hey)
+print(type(hey))
+print("------------------------\n" , hie)
+print(type(hie))
+
+# ==================-------------------------------------------------
+#pov: studying python newly...
+
+a = 6
+print(a ** 2) # 36
+
+b = 5
+print(b ** 3) #125
+
+side = 34
+side2 = 28
+print(side * side2)
+
+n1 = 34
+n2 = 28
+[n1, n2] = [n2, n1]
+print("----------------\n\nfirst my n1 ", n1)
+print("second", n2)
+
+n3 = 30
+n4 = 40
+temp = n3
+n3 = n4
+n4 = temp
+print(n3, n4)
+
