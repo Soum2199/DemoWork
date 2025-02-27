25c9a86451a674e753fd5179cca4f5ac3c1cbfe0
Soum2199 <s1646517887@gmail.com>
2025-02-27 13:47:46 +0530

    Modified JavaScript file spotted

--- actions.js
+++ actions.js
@@ -1,4 +1,4 @@
-// Action Types
+// Action Types mentioned below are:
 export const ADD_PRODUCT = "ADD_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
