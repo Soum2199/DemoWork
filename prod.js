af362c5ece84fe4a75ff65fb7de8919a6c0db793
Soum2199 <s1646517887@gmail.com>
2025-07-30 08:44:07 +0530

    JS file pushed

---
+++ prod.js
@@ -0,0 +1,57 @@
+import hii from "https://instamart-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/cb26c44b691754d7ca67f67d824e8411"
+import hey from "https://shoopy.b-cdn.net/340140/aashirvaad-atta-1702468645457_SKU-0836_0.jpg?format=webp"
+import hello from "https://images.ctfassets.net/sabbecbbwaz3/75LNk0YBkx43OwldxrGUc6/eaa574a84c4eceef2910dd9b567edd52/VVR_50g_Jar_Closed__1_.png?w=262"
+
+const prod = [
+    {
+        names:"Society Tea",
+        price: 42,
+        descript:"Hii all",
+        rating: 5.7,
+        image: hii
+    },
+    {
+        names:"Aashirvad Aata",
+        price: 250,
+        descript:"Hello world",
+        rating: 6.9,
+        image: hey
+    },
+    {
+        names:"Vicks Vapour Rub",
+        price: 95,
+        descript:"Hola amigos",
+        rating: 7.8,
+        image: hello
+    },
+    {
+        names:"Britannia Marie Gold Biscuit",
+        price: 240,
+        descript:"good day",
+        rating: 8.7,
+        image: "https://www.bbassets.com/media/uploads/p/l/220244_9-britannia-marie-gold-biscuits.jpg"
+    },
+    {
+        names:"Fair and Lovely Makeup Cream",
+        price: 104,
+        descript:"happy sunday",
+        rating: 7.2,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9ihoCrL2sLyUModSz063Hj_PQwp8_Q8vWYg&s"
+    },
+    {
+        names:"Kissan Tomato Sauce Ketchup",
+        price: 34,
+        descript:"thanks a tone, for the sauce",
+        rating: 8.0,
+        image: "https://5.imimg.com/data5/OW/FI/TP/SELLER-5099161/kissan-tomato-ketchup-500x500.jpg"
+    },
+    {
+        names:"Tira Refined Sunflower Oil",
+        price: 600,
+        descript:"So very good for health, its a boon indeed",
+        rating: 6.8,
+        image: "https://5.imimg.com/data5/SELLER/Default/2024/4/408939312/PJ/EU/OL/3572809/5l-tirupati-sunpride-refined-sunflower.png"
+    }
+];
+
+export default prod;
