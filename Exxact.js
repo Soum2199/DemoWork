562ed4fd47956cf2ab520c5004c79cdd0bbd1cf0
Soum2199 <s1646517887@gmail.com>
2025-09-09 13:08:45 +0530

    My js datas

---
+++ Exxact.js
@@ -0,0 +1,99 @@
+export const hey = [  
+  {
+      "id": 91,
+      "question": "Which programming language is commonly used for web development?",
+      "options": ["Python", "Java", "C++", "JavaScript"],
+      "correct": 3,
+      "category": "Technology"
+    },
+    {
+      "id": 92,
+      "question": "What is the most commonly spoken language in South America?",
+      "options": ["Portuguese", "Spanish", "French", "English"],
+      "correct": 1,
+      "category": "Language"
+    },
+    {
+      "id": 93,
+      "question": "Which artist is famous for painting 'Starry Night'?",
+      "options": [
+        "Claude Monet",
+        "Pablo Picasso",
+        "Vincent van Gogh",
+        "Leonardo da Vinci"
+      ],
+      "correct": 2,
+      "category": "Art"
+    },
+    {
+      "id": 94,
+      "question": "What is the main ingredient in traditional miso soup?",
+      "options": ["Soybeans", "Rice", "Noodles", "Seaweed"],
+      "correct": 0,
+      "category": "Food"
+    },
+    {
+      "id": 95,
+      "question": "What type of blood cells help fight infections in the human body?",
+      "options": ["Red blood cells", "Platelets", "White blood cells", "Plasma"],
+      "correct": 2,
+      "category": "Science"
+    },
+    {
+      "id": 96,
+      "question": "Which continent has the most countries?",
+      "options": ["Asia", "Europe", "Africa", "South America"],
+      "correct": 2,
+      "category": "Geography"
+    },
+    {
+      "id": 97,
+      "question": "Who wrote 'The Catcher in the Rye'?",
+      "options": [
+        "J.D. Salinger",
+        "Ernest Hemingway",
+        "Harper Lee",
+        "F. Scott Fitzgerald"
+      ],
+      "correct": 0,
+      "category": "Literature"
+    },
+    {
+      "id": 98,
+      "question": "Which country won the FIFA World Cup in 2018?",
+      "options": ["Germany", "Brazil", "France", "Argentina"],
+      "correct": 2,
+      "category": "Sports"
+    },
+    {
+      "id": 99,
+      "question": "Which war was primarily fought between the North and South regions of the United States?",
+      "options": ["Revolutionary War", "World War I", "Civil War", "Vietnam War"],
+      "correct": 2,
+      "category": "History"
+    },
+    {
+      "id": 100,
+      "question": "What does 'CPU' stand for in computing?",
+      "options": [
+        "Central Processing Unit",
+        "Computer Processing Utility",
+        "Core Processing Unit",
+        "Central Power Unit"
+      ],
+      "correct": 0,
+      "category": "Technology"
+    },
+    {
+      "id": 101,
+      "question": "What does 'CSS' stand for in web development?",
+      "options": [
+        "Centre Secondary Sites",
+        "Cascading Style Sheet",
+        "Core Student Source",
+        "Central Science School"
+      ],
+      "correct": 1,
+      "category": "Technology"
+    }
+]
---
+++ SirsMovie.js
@@ -0,0 +1,117 @@
+[
+  {
+      "id": 1,
+      "movie_name": "Dune2",
+      "image_url": "https://4kwallpapers.com/images/wallpapers/dune-2-movie-poster-2732x2732-14717.jpg",
+      "description": "In Dune, Paul Atreides joins forces with Chani and the Fremen to avenge his family's downfall. Faced with a pivotal decision, he balances love and duty to avert a catastrophic destiny.",
+      "genre": "Action/Fantasy/Sci-Fi",
+      "censor": "U/A",
+      "director": "Denis Villeneuve",
+      "rating": "8.5/10",
+      "cast": ["Timothée Chalamet", "Rebecca Ferguson", "Zendaya"]
+  },
+  {
+      "id": 2,
+      "movie_name": "Oppenheimer",
+      "image_url": "https://images4.alphacoders.com/132/1323605.jpeg",
+      "description": "Oppenheimer follows the life of J. Robert Oppenheimer, the physicist who led the Manhattan Project, as he grapples with the moral and ethical implications of creating the atomic bomb, ultimately shaping the course of modern history.",
+      "genre": "Biographical/Drama",
+      "censor": "U",
+      "director": "Christopher Nolan",
+      "rating": "8.3/10",
+      "cast": ["Cillian murphy" , "Robert Downey Jr." , "Emily Blunt"]
+  },
+  {
+      "id": 3,
+      "movie_name": "OG",
+      "image_url": "https://moviegalleri.net/wp-content/uploads/2024/02/Pawan-Kalyan-OG-Movie-Release-Date-on-September-27th-Poster-HD.jpg",
+      "description": "Ojas Gambheera, a feared gangster, returns to Mumbai after a decade in exile, determined to reclaim his power and finish what he started.",
+      "genre": "Action/Drama",
+      "censor": "U/A",
+      "director": "Sujeeth",
+      "rating": "6.5/10",
+      "cast": ["Pawan Kalyan", "Emraan Hashmi" , "Priyanka Mohan"]
+  },
+  {
+      "id": 4,
+      "movie_name": "Baahubali: The Conclusion",
+      "image_url": "https://e1.pxfuel.com/desktop-wallpaper/875/703/desktop-wallpaper-prabhas-baahubali-the-conclusion-movie-ultra-posters-baahubali-the-beginning.jpg",
+      "description": "The second installment of the epic Baahubali saga.",
+      "genre": "Action/Drama",
+      "censor": "U/A",
+      "director": "S.S. Rajamouli",
+      "rating": "8.2/10",
+      "cast": ["Prabhas", "Rana Daggubati", "Anushka Shetty"]
+  },
+  {
+      "id": 5,
+      "movie_name": "Interstellar",
+      "image_url": "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+      "description": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+      "genre": "Sci-Fi/Drama",
+      "censor": "PG-13",
+      "director": "Christopher Nolan",
+      "rating": "8.7/10",
+      "cast": ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"]
+  },
+  {
+      "id": 6,
+      "movie_name": "Arjun Reddy",
+      "image_url": "https://i.pinimg.com/originals/9d/84/5e/9d845e298cb90b177bf008f28abb1030.jpg",
+      "description": "A brilliant medical student spirals into self-destruction after an intense breakup.",
+      "genre": "Drama/Romance",
+      "censor": "A",
+      "director": "Sandeep Reddy Vanga",
+      "rating": "7.9/10",
+      "cast": ["Vijay Deverakonda", "Shalini Pandey"]
+  },
+  {
+      "__v": 0,
+      "id": 7,
+      "movie_name": "The Dark Knight",
+      "image_url": "https://i.pinimg.com/originals/41/83/28/41832844a09a83213c8a29fc577ca7c8.jpg",
+      "description": "Batman faces off against the Joker, a criminal mastermind set on wreaking havoc on Gotham City.",
+      "genre": "Action/Crime/Drama",
+      "censor": "PG-13",
+      "director": "Christopher Nolan",
+      "rating": "9/10",
+      "cast": ["Christian Bale", "Heath Ledger", "Aaron Eckhart"]
+  },
+  {
+      "__v": 0,
+      "id": 8,
+      "movie_name": "Rangasthalam",
+      "image_url": "https://e0.pxfuel.com/wallpapers/814/824/desktop-wallpaper-ram-charan-s-rangasthalam-is-first-non-baahubali-telugu-film-to-achieve-it-tollywood-thumbnail.jpg",
+      "description": "A man born with dwarfism sets out to prove himself in a small village ruled by a tyrannical president.",
+      "genre": "Drama/Action",
+      "censor": "U/A",
+      "director": "Sukumar",
+      "rating": "8.2/10",
+      "cast": ["Ram Charan", "Samantha Akkineni"]
+  },
+  {
+      "__v": 0,
+      "id": 9,
+      "movie_name": "Inception",
+      "image_url": "https://c4.wallpaperflare.com/wallpaper/764/590/391/inception-leonardo-dicaprio-movie-posters-2400x3500-entertainment-movies-hd-art-wallpaper-preview.jpg",
+      "description": "A thief who enters the dreams of others to steal their secrets must plant an idea in someone's mind.",
+      "genre": "Action/Sci-Fi/Thriller",
+      "censor": "PG-13",
+      "director": "Christopher Nolan",
+      "rating": "8.8/10",
+      "cast": ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"]
+  },
+  {
+      "__v": 0,
+      "id": 10,
+      "movie_name": "Avatar",
+      "image_url": "https://i.pinimg.com/736x/8b/2f/a6/8b2fa6fb94810cd0d335b479896f7fc8.jpg",
+      "description": "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+      "genre": "Action/Adventure/Fantasy",
+      "censor": "PG-13",
+      "director": "James Cameron",
+      "rating": "7.9/10",
+      "cast": ["Sam Worthington", "Zoe Saldana", "Sigourney Weaver"]
+  }
+]
+
