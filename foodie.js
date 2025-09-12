1c6aeda46970ae0bb3d09b7f6d79e9e1a5948f05
Soum2199 <s1646517887@gmail.com>
2025-09-12 21:51:53 +0530

    added a js file

---
+++ foodie.js
@@ -0,0 +1,73 @@
+export const foodLists = [
+  {
+    foodId: 1,
+    foodCost: 299,
+    foodRate: 3.2,
+    foodNames: "Salads",
+    foodImg:
+      "https://media.istockphoto.com/id/503638866/photo/plate-with-salad.jpg?s=1024x1024&w=is&k=20&c=r-y2aISL2J1Q9LTham9au4fLf8sRroNn-R2JZrVZKMA=",
+  },
+  {
+    foodId: 2,
+    foodCost: 499,
+    foodRate: 4.1,
+    foodNames: "Cakes",
+    foodImg:
+      "https://media.istockphoto.com/id/1367889053/photo/pancakes-stacked-with-strawberries-topped-with-chocolate-sauce-and-yoghurt.jpg?s=1024x1024&w=is&k=20&c=KFy-27Bdoo6lkzQ2I1DaKWDWqrgcJmF1GoSHjVlYJlw=",
+  },
+  {
+    foodId: 3,
+    foodCost: 249,
+    foodRate: 3.4,
+    foodNames: "Noodles",
+    foodImg:
+      "https://media.istockphoto.com/id/641820384/photo/noodles-in-green-bowl.jpg?s=1024x1024&w=is&k=20&c=WKNH8bZPtkG6Kb0OH1wMN6taPdY5wrjQ6-q9tKbxNjM=",
+  },
+  {
+    foodId: 4,
+    foodCost: 199,
+    foodRate: 2.9,
+    foodNames: "Sandwich",
+    foodImg:
+      "https://media.istockphoto.com/id/2046217346/photo/indian-plain-sandwich-with-vegetable-and-sauce-in-plate-india-closeup.jpg?s=1024x1024&w=is&k=20&c=Ojk7tG4xBs3r0_4OW9t3AtHBpSTEVgA0hL60TDK-tO4=",
+  },
+  {
+    foodId: 5,
+    foodCost: 169,
+    foodRate: 4.3,
+    foodNames: "Juice",
+    foodImg:
+      "https://media.istockphoto.com/id/185296663/photo/glass-of-orange-juice.jpg?s=1024x1024&w=is&k=20&c=37hqfl3dp7otrWZpNW-Dws7mB3REgRfvL5koYHy5PvI=",
+  },
+  {
+    foodId: 6,
+    foodCost: 399,
+    foodRate: 4.7,
+    foodNames: "Dessert",
+    foodImg:
+      "https://media.istockphoto.com/id/1137312508/photo/assortment-of-products-with-high-sugar-level.jpg?s=1024x1024&w=is&k=20&c=31JIGszsx0mvGauCTxUED2epIbocfo4HIj3v9m_fjSQ=",
+  },
+  {
+    foodId: 7,
+    foodCost: 199,
+    foodRate: 4.5,
+    foodNames: "Ice-cream",
+    foodImg:
+      "https://media.istockphoto.com/id/166121533/photo/ice-cream-in-the-cone.jpg?s=1024x1024&w=is&k=20&c=2O1pCjg6JzLS9xa5jYguguhHjCsuc-L5siTHi5ynrdw=",
+  },
+{
+    foodId: 8,
+    foodCost: 449,
+    foodRate: 4.8,
+    foodNames: "Pizza",
+    foodImg:
+      "https://media.istockphoto.com/id/153444470/photo/pizza.jpg?s=1024x1024&w=is&k=20&c=TYRIpKiWvR0aKKBspWjesTaI-H71F8Gdd1dH5kE_qao=",
+  },
+{
+    foodId: 9,
+    foodCost: 369,
+    foodRate: 4.7,
+    foodNames: "Milkshakes",
+    foodImg: "https://media.istockphoto.com/id/2175043434/photo/chocolate-cookie-milkshake-with-almonds-and-strawberry-served-in-jar-isolated-on-wooden-board.jpg?s=1024x1024&w=is&k=20&c=-rZAsiyCN4Ta0g1x7ISEEpvk16MpFVeEIcm_G5CDTEg=",
+}
+];
