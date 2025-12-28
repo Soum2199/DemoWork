e4c8257ac908b4224b5a3750ce796a46f52ac37e
Soum2199 <s1646517887@gmail.com>
2025-12-28 22:34:12 +0530

    Lastly JavaScript commit

---
+++ Demo2.js
@@ -0,0 +1,15 @@
+
+// console.log("Try programiz.pro");
+// let a = 5
+// a++
+// console.log(a)
+//   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+let hi = Math.floor(Math.random()*100);
+console.log(hi)
+if(hi > 18){
+    console.log("wow")
+}
+else{
+    console.log("Awwie.....")
+}
