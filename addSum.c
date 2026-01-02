faf1fae89d08ecd9e1aa00e1448d454f4637a9ca
Soum2199 <s1646517887@gmail.com>
2026-01-02 23:30:49 +0530

    My 1st C commit

---
+++ addSum.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a, b, c;
+    printf("Enter the numbers:\n");
+    scanf("%d  %d", &a, &b);
+    c = a + b;
+    printf("The sum is %d", c);
+    return 0;
+}
