74f43c11eccbfd52ac7118413f68e72f5050b5b0
Soum2199 <s1646517887@gmail.com>
2026-01-11 17:08:45 +0530

    Bubble sorting java

---
+++ BubbleSort.java
@@ -0,0 +1,20 @@
+// Java Bubble Sort - GFG
+
+class Solution {
+    public void bubbleSort(int[] arr) {
+        // code here
+        for(int i=0; i<arr.length-1; i++){
+    for(int j=0; j<arr.length-1; j++){
+        if(arr[j] > arr[j+1]){
+            int temp = arr[j+1];
+            arr[j+1] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
+        return;
+    }
+}
+
+//TC = O(N^2)
+// SC = O(1)
